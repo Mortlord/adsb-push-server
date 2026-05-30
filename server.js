a9db418da5bbdf14c16ad2a000063f8e14b30588
Mortlord <martin.staaden@gmail.com>
2026-05-30 16:58:58 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const COOLDOWN_MS = 5 * 60 * 1000; // 5 Minuten pro Callsign
 const STATE_FILE   = '/data/userstate.json';
 const HISTORY_FILE = '/data/flighthistory.json';
 
+// Sicherstellen dass /data existiert
+if (!fs.existsSync('/data')) fs.mkdirSync('/data', { recursive: true });
+
 function loadJSON(file, def) {
   try { return JSON.parse(fs.readFileSync(file, 'utf8')); }
   catch { return def; }
